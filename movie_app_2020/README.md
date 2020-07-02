ff064e30db664f292ca8dffaa59456a50bf062c7
unknown <sw.onandon@gmail.com>
2020-07-02 11:30:37 +0900

    0702 : fin Chapter #2.1 Reusable Components with JSX + Props

--- movie_app_2020/README.md
+++ movie_app_2020/README.md
@@ -2,5 +2,8 @@
 
 React JS Fundamentals Course! 
 
-ReactJS로 영화 웹 서비스 만들기 강의 
-> #2.0 Creating your first React Component 까지의 내용을 담은 소스코드 
+
+#### ReactJS로 영화 웹 서비스 만들기 강의 
+
+-[x] \#2.0 Creating your first React Component 까지 완료, 커밋 
+-[x] \#2.1 Reusable Components with JSX + Props 
--- movie_app_2020/src/App.js
+++ movie_app_2020/src/App.js
@@ -1,13 +1,48 @@
 import React from 'react';
-import Potato  from './potato';
+
+function Potato(){
+    return <h2>I like Potato</h2>
+}
+
+// case 1. props
+function Food(props){
+    console.log(props) // > {name: "kimchi"}
+    console.log(props.name) // > kimchi
+    return <h3> What Food? </h3>
+}
+// case 2.  {props의 속성(이름)}
+function Color({name}){
+    return <h4>Beautiful {name} </h4>
+}
+
+// case 3.  props.속성
+function PropTest(props){
+    return <h4>{props.favo}</h4> // cake
+}
 
 function App() {
   return (
     <div className="App">
         <h1>Hello!</h1>
-        <Potato />
+
+        <Potato/>
+
+        <Food name ='kimchi' />
+        {/*props가 name인 Food가 불린다.*/}
+
+        <Color name = 'Black' />
+        <Color name = 'Blue' />
+        <Color name = 'Yellow' />
+        <Color name = 'Green' />
+
+        <PropTest
+            favo = 'cake'
+            sth={true}
+            bla={['hello',1,2,3,true]} />
+        {/* 여러 props를 정의하여 Food를 부를수도 있다. */}
     </div>
   );
 }
-
+// jsx에서는 컴포넌트에서 정보를 보낼 수 있다.
+// 리액트의 장점 하나 : 컴포넌트를 만들고, 무한 재사용할 수 있다는 것
 export default App;
